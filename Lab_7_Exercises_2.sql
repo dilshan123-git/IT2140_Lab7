cb17b679fff2a7e1caa182c31e8275451cea6a94
dilshan123-git <it20279684@my.sliit.lk>
2026-09-22 21:14:42 +0530

    Add files via upload

---
+++ Lab_7_Exercises_2.sql
@@ -0,0 +1,35 @@
+USE moviedb;
+
+DROP PROCEDURE IF EXISTS UpdateMovieStarRanks;
+DROP TRIGGER IF EXISTS trg_update_star_rank;
+
+DELIMITER //
+
+CREATE PROCEDURE UpdateMovieStarRanks()
+BEGIN
+    UPDATE MovieStar m
+    SET m.`rank` = (
+        SELECT COUNT(*) 
+        FROM StarsIn s 
+        WHERE s.starname = m.name 
+          AND s.role = 'Lead'
+    );
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER trg_update_star_rank
+AFTER INSERT ON StarsIn
+FOR EACH ROW
+BEGIN
+    IF NEW.role = 'Lead' THEN
+        UPDATE MovieStar
+        SET `rank` = `rank` + 1
+        WHERE name = NEW.starname;
+    END IF;
+END //
+
+DELIMITER ;
